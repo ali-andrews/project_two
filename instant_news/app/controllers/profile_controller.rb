6b0054bede7da5f3adfcceb0e1ac921564277fe0
ali-andrews <alexandra.andrews2010@gmail.com>
2015-11-02 09:51:25 +0000

    first commit

---
+++ instant_news/app/controllers/profile_controller.rb
@@ -0,0 +1,6 @@
+class ProfileController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+end
---
+++ instant_news/db/migrate/20151101200858_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.datetime :date
+      t.string :location
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
