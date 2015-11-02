df071bc77eca01313f2efca5eb51b6bdc7b04a18
ali-andrews <alexandra.andrews2010@gmail.com>
2015-11-02 10:32:34 +0000

    Added photo url to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101200858) do
+ActiveRecord::Schema.define(version: 20151102103125) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151101200858) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "photo_url"
   end
 
   create_table "users", force: :cascade do |t|
