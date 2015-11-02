a956ec39f98bac2dcfcd7c2a6fd13b7fbe64cec7
ali-andrews <alexandra.andrews2010@gmail.com>
2015-11-02 11:34:31 +0000

    Before adding foundation

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ end
 
 gem 'devise'
 
+gem 'foundation-rails'
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
         u.permit(:first_name, :last_name,
           :email, :password, :password_confirmation)
       end  
+      devise_parameter_sanitizer.for(:account_update) do |u|
+        u.permit(:username, :first_name, :last_name,
+         :email, :password, :password_confirmation, :current_password)
+      end
     end
 end
 
